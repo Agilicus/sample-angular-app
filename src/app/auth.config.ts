5bd5ac0d181f8a8361561be18a48320a7e53794b
Kyle Larose <kyle@agilicus.com>
2019-09-19 13:07:23 -0400

    fix: override the client id as well
    The client id is used to form the audience, which needs to match we get
    from the issuer. Whoops.

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -1,19 +1,23 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
 import { environment as env } from '../environments/environment'
 
-function authHost(): string {
+function baseHost(): string {
   if (env['overrideDomain']) {
-    return "auth." + env['overrideDomain'];
+    return env['overrideDomain'];
   }
 
   const hostname = window.location.hostname;
   const firstPeriod = hostname.indexOf(".");
-  return "auth" + "." + hostname.substring(firstPeriod + 1);
+  return hostname.substring(firstPeriod + 1);
+}
+
+function authHost(): string {
+    return "auth." + baseHost();
 }
 
 export const authConfig: AuthConfig = {
   issuer: 'https://' + authHost() + '/',
   redirectUri: window.location.origin + '/',
-  clientId: 'egov.city',
+  clientId: baseHost(),
   scope: 'openid profile email federated:id',
 }
